c6f1ba609c3190afc375388a8475c48e262e77a8
lisaorii <lisa.orii@gmail.com>
2020-09-29 22:33:26 -0400

    Update record.js

--- js/record.js
+++ js/record.js
@@ -1,9 +1,9 @@
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
 
-var gumStream; 						//stream from getUserMedia()
-var rec; 							//Recorder.js object
-var input; 							//MediaStreamAudioSourceNode we'll be recording
+var gumStream;            //stream from getUserMedia()
+var rec;              //Recorder.js object
+var input;              //MediaStreamAudioSourceNode we'll be recording
 var url;
 
 var au = document.createElement('audio');
@@ -14,10 +14,6 @@ var link = document.createElement('a');
 // shim for AudioContext when it's not avb.
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext //audio context to help us record
-
-// const recordButton = document.querySelector('.record');
-// const stopButton = document.querySelector('.stop');
-// const pauseButton = document.querySelector('.pause');
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 var pauseButton = document.getElementById("pauseButton");
@@ -28,222 +24,208 @@ stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
 
 function startRecording() {
-    console.log("recordButton clicked");
-    recordButton.style.background = "red";
-    stopButton.style.background = "";
-    pauseButton.style.background = "";
+  console.log("recordButton clicked");
+  recordButton.style.background = "red";
+  stopButton.style.background = "";
+  pauseButton.style.background = "";
 
-	/*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
+  /*
+     Simple constraints object, for more advanced audio features see
+     https://addpipe.com/blog/audio-constraints-getusermedia/
+   */
 
-    var constraints = { audio: true, video:false }
+  var constraints = { audio: true, video:false }
 
- 	/*
-    	Disable the record button until we get a success or fail from getUserMedia()
-	*/
+  /*
+     Disable the record button until we get a success or fail from getUserMedia()
+   */
 
-	recordButton.disabled = true;
-	stopButton.disabled = false;
-	pauseButton.disabled = false
+  recordButton.disabled = true;
+  stopButton.disabled = false;
+  pauseButton.disabled = false
 
-	/*
-    	We're using the standard promise based getUserMedia()
-    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
+  /*
+     We're using the standard promise based getUserMedia()
+     https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+   */
 
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
+    /*
+       create an audio context after getUserMedia is called
+       sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+       the sampleRate defaults to the one set in your OS for your playback device
 
-		*/
-		audioContext = new AudioContext();
+     */
+    audioContext = new AudioContext();
 
-		//update the format
-		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+    //update the format
+    document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
 
-		/*  assign to gumStream for later use  */
-		gumStream = stream;
+    /*  assign to gumStream for later use  */
+    gumStream = stream;
 
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
+    /* use the stream */
+    input = audioContext.createMediaStreamSource(stream);
 
-		/*
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
-		rec = new Recorder(input,{numChannels:1})
+    /*
+       Create the Recorder object and configure to record mono sound (1 channel)
+       Recording 2 channels  will double the file size
+     */
+    rec = new Recorder(input,{numChannels:1})
 
-		//start the recording process
-		rec.record()
+    //start the recording process
+    rec.record()
 
-		console.log("Recording started");
+    console.log("Recording started");
 
-	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
-    	recordButton.disabled = false;
-    	stopButton.disabled = true;
-    	pauseButton.disabled = true
-	});
+  }).catch(function(err) {
+    //enable the record button if getUserMedia() fails
+    recordButton.disabled = false;
+    stopButton.disabled = true;
+    pauseButton.disabled = true
+  });
 }
 
 function pauseRecording(){
-	console.log("pauseButton clicked rec.recording=",rec.recording );
-	if (rec.recording){
-		//pause
-		rec.stop();
-        pauseButton.innerHTML="Resume";
+  console.log("pauseButton clicked rec.recording=",rec.recording );
+  if (rec.recording){
+    //pause
+    rec.stop();
+    pauseButton.innerHTML="Resume";
 
-	}else{
-		//resume
-		rec.record()
-		pauseButton.innerHTML="Pause";
+  }else{
+    //resume
+    rec.record()
+    pauseButton.innerHTML="Pause";
 
-    }
-    recordButton.style.background = "";
-    stopButton.style.background = "";
-    pauseButton.style.background = "red";
+  }
+  recordButton.style.background = "";
+  stopButton.style.background = "";
+  pauseButton.style.background = "red";
 }
 
 function stopRecording() {
-    console.log("stopButton clicked");
-    recordButton.style.background = "";
-    stopButton.style.background = "red";
-    pauseButton.style.background = "";
+  console.log("stopButton clicked");
+  recordButton.style.background = "";
+  stopButton.style.background = "red";
+  pauseButton.style.background = "";
 
-	//disable the stop button, enable the record too allow for new recordings
-	stopButton.disabled = true;
-	recordButton.disabled = false;
-	pauseButton.disabled = true;
+  //disable the stop button, enable the record too allow for new recordings
+  stopButton.disabled = true;
+  recordButton.disabled = false;
+  pauseButton.disabled = true;
 
-	//reset button just in case the recording is stopped while paused
-	pauseButton.innerHTML="Pause";
+  //reset button just in case the recording is stopped while paused
+  pauseButton.innerHTML="Pause";
 
-	//tell the recorder to stop the recording
-	rec.stop();
+  //tell the recorder to stop the recording
+  rec.stop();
 
-	//stop microphone access
-	gumStream.getAudioTracks()[0].stop();
+  //stop microphone access
+  gumStream.getAudioTracks()[0].stop();
 
-	// var arrayBuffer = new ArrayBuffer(100);
-	// var uint8Array = new Uint8Array(arrayBuffer);
-	// for (var i = 0; i < 100; i++) {
-	// 	uint8Array[i] = i;
-	// }
-	// blob = new Blob([uint8Array], {  type: 'audio/ogg; codecs=opus'  });
+  // var arrayBuffer = new ArrayBuffer(100);
+  // var uint8Array = new Uint8Array(arrayBuffer);
+  // for (var i = 0; i < 100; i++) {
+  //  uint8Array[i] = i;
+  // }
+  // blob = new Blob([uint8Array], {  type: 'audio/ogg; codecs=opus'  });
 
-	//create the wav blob and pass it on to createDownloadLink
-	rec.exportWAV(createDownloadLink);
+  //create the wav blob and pass it on to createDownloadLink
+  rec.exportWAV(createDownloadLink);
 }
 
 function createDownloadLink(blob) {
 
-	url = URL.createObjectURL(blob);
-	// $('div').html(blobVal);
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
+  url = URL.createObjectURL(blob);
 
-	console.log(url);
+  var filename = new Date().toISOString();
 
-	// Recorder.setupDownload = function(blob, filename) {
-	// 	var fd = new FormData();
-	// 	fd.append('fname', filename);
-	// 	fd.append('data', blob);
-	// 	$.ajax({
-	// 	  type: 'POST',
-	// 	  url: '/upload.php',
-	// 	  data: fd,
-	// 	  processData: false,
-	// 	  contentType: false
-	// 	}).done(function(data) {
-	// 	  console.log(data);
-	// 	  console.log(url);
-	// 	});
-	//   }
+  console.log(url);
 
+  //add controls to the <audio> element
+  au.controls = true;
+  au.src = url;
 
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
+  //save to disk link
+  link.href = url;
+  // console.log(link.href);
+  link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+  link.innerHTML = "Save to disk";
 
-	//save to disk link
-    link.href = url;
-    // console.log(link.href);
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
+  //add the new audio element to li
+  li.appendChild(au);
 
-	//add the new audio element to li
-	li.appendChild(au);
+  //add the filename to the li
+  li.appendChild(document.createTextNode(filename+".wav "))
 
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
+  //add the save to disk link to li
+  li.appendChild(link);
 
-	//add the save to disk link to li
-	li.appendChild(link);
-
-	//upload link
-	var upload = document.createElement('a');
+  //upload link
+  var upload = document.createElement('a');
   upload.href="#";
-	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-          var xhr = new XMLHttpRequest(); //HAVING ISSUES WITH XMLHTTPREQUEST
-          console.log("request");
-		  xhr.onload = function(e) {
-              console.log("on load");
-		      if(this.readyState === 4) {
-		          console.log("Server returned: ",e.target.responseText);
-		      }
-          };
-          console.log("done");
-		  var fd=new FormData();
-		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.php",true);
-		  xhr.send(fd);
-	})
-	li.appendChild(document.createTextNode (" "))//add a space in between
-	li.appendChild(upload)//add the upload link to li
+  upload.innerHTML = "Upload";
+  upload.addEventListener("click", function(event){
+    var xhr = new XMLHttpRequest(); //HAVING ISSUES WITH XMLHTTPREQUEST
+    console.log("request");
+    xhr.onload = function(e) {
+      console.log("on load");
+      if(this.readyState === 4) {
+        console.log("Server returned: ",e.target.responseText);
+      }
+    };
+    console.log("done");
+    var fd=new FormData();
+    fd.append("audio_data",blob, filename);
+    xhr.open("POST","upload.php",true);
+    xhr.send(fd);
+  })
+  li.appendChild(document.createTextNode (" "))//add a space in between
+  li.appendChild(upload)//add the upload link to li
 
-	//add the li element to the ol
-    recordingsList.appendChild(li);
-
+  //add the li element to the ol
+  recordingsList.appendChild(li);
 }
 
-  const pitchShift = new Tone.PitchShift().toDestination();　//Connect the output to the context's destination node.
-  const player = new Tone.GrainPlayer(url).connect(pitchShift);
-  player.loop = true; //creates a looped callback at the specified interval
-  console.log("url uploaded; starting pitchshift");
+let loadedCount = 0;
+let player = null;
+let pitchShift = null;
 
+// 音源読み込み後にしたい処理を記述した関数
+function loadedEvent() {
+  if(loadedCount == 0) {
+    setTimeout( loadedEvent, 100);
+    return;
+  }
+
+  pitchShift = new Tone.PitchShift().toDestination();　//Connect the output to the context's destination node.
   const toneFFT = new Tone.FFT(); //Get the current frequency data of the connected audio source using a fast Fourier transform.
   pitchShift.connect(toneFFT); //connect to toneFFT
   fft({
-        parent: document.querySelector("#content"),
-      tone: toneFFT,
+    parent: document.querySelector("#content"),
+    tone: toneFFT,
   });
 
+  player.connect(pitchShift);
+  player.start();
+}
 
-  // bind the interface
-  document.querySelector("tone-play-toggle").addEventListener("start", () => player.start());
-
-  // console.log("play");
-  document.querySelector("tone-play-toggle").addEventListener("stop", () => player.stop());
-  // console.log("stop");
-  // document.querySelector("tone-play-toggle").addEventListener('start', () => {
-  // 	debugger;
-  // });
-
-  document.getElementByClassName("pitch").addEventListener("input", e => {
-      pitchShift.pitch = parseFloat(e.target.value);
+// bind the interface
+document.querySelector("tone-play-toggle").addEventListener("start", () => {
+  console.log(url);
+  player = new Tone.Player(url, function(){
+    loadedCount ++;
   });
+  player.loop = true; //creates a looped callback at the specified interval
 
-  // document.getElementByClassName("volume").addEventListener("input", e => {
-  //     pitchShift.pitch = parseFloat(e.target.value);
-  // });
-  //
-  // document.getElementByClassName("speed").addEventListener("input", e => {
-  //     pitchShift.pitch = parseFloat(e.target.value);
-  // });
+  loadedEvent();
+});
+
+document.querySelector("tone-play-toggle").addEventListener("stop", () => player.stop());
+
+document.getElementsByClassName("pitch")[0].addEventListener("input", e => {
+  pitchShift.pitch = parseFloat(e.target.value);
+});
